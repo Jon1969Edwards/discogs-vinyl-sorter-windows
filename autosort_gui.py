6fac1e58001b1373db97c4899320424feaf6a82f
Jon Edwards <jon1969edwards@gmail.com>
2026-01-31 21:17:11 +0100

    Initialize wishlist rows to ensure proper functionality before use

--- autosort_gui.py
+++ autosort_gui.py
@@ -1976,6 +1976,7 @@ class App:
 
     # Populate wishlist
     from core.wishlist import load_wishlist, remove_from_wishlist
+    self._wishlist_rows = []  # Always initialize before any use
     def refresh_wishlist_tree():
       wishlist_tree.delete(*wishlist_tree.get_children())
       from types import SimpleNamespace
